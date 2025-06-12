7f67fdd74c38567345ce9e3dd975e6120dc2d849
Ertaoz Bregvadze <labadze156@gmail.com>
2025-06-12 15:56:48 +0400

    1

---
+++ app/components/button/Button.tsx
@@ -0,0 +1,9 @@
+type Props = {
+    title: string;
+}
+
+export default (props: Props) => {
+    return (
+        <div>{props.title}</div>
+    )
+}
---
+++ app/contuctus/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css"
+
+export default () => {
+    return (
+        <div className={styles.container}> 
+            <h1>Contact Us</h1>
+        </div>
+    )
+}
